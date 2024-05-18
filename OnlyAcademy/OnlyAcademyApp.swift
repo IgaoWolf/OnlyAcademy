326d180fc4c9285e8315c761eeb69de11f93ecab
Igor Augusto Wolf <igaowolf@gmail.com>
2024-05-18 18:00:59 -0300

    Initial Commit

---
+++ OnlyAcademy/OnlyAcademyApp.swift
@@ -0,0 +1,32 @@
+//
+//  OnlyAcademyApp.swift
+//  OnlyAcademy
+//
+//  Created by Igor Augusto Wolf on 18/05/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OnlyAcademyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
